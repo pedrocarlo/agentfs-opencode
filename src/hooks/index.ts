d04a3410412a1f302e05e4bcca8481df192f1f46
pedrocarlo <pedro.muniz.carlo@gmail.com>
2025-12-28 15:54:49 -0300

    remove tool tracking code

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,3 @@
 export { registerCleanupHandlers } from "./cleanup"
 export { createPathRewriteAfterHandler, createPathRewriteHandler } from "./path-rewrite"
 export { createSessionHandler } from "./session"
-export {
-	createToolExecuteAfterHandler,
-	createToolExecuteBeforeHandler,
-} from "./tool-tracking"
